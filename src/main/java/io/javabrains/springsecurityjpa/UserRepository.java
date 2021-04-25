930acf783359fb80c51447afda30b72736d56e33
Wissem Yahiaoui <w.yahiaoui@ev-digitalinvest.de>
2021-04-25 05:44:33 +0200

    unit

---
+++ src/main/java/io/javabrains/springsecurityjpa/UserRepository.java
@@ -0,0 +1,12 @@
+package io.javabrains.springsecurityjpa;
+
+import io.javabrains.springsecurityjpa.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
